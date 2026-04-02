f5400390621bd755f474058cae45f5f6e437be45
Pierluigi92 <pierluigi.mancino@hotmail.com>
2026-04-02 23:11:36 +0200

    added function 'playRound' and case insensitive at 'getHumanChoice'

--- javascript.js
+++ javascript.js
@@ -1,9 +1,15 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
 function getComputerChoice() {
     return Math.floor(Math.random()* 3);
 }
 
 function getHumanChoice() {
-let scelta = prompt("fai la tua scelta: 's' per sasso, 'c' per carta e 'f' per forbice ?");  
+let scelta = prompt("fai la tua scelta: 's' per sasso, 'c' per carta e 'f' per forbice ?")?.toLowerCase();  
 if (scelta === 's') { 
     return 0; 
 } else if (scelta === 'c') { 
@@ -15,4 +21,17 @@ if (scelta === 's') {
 }
 }
 
-console.log(getHumanChoice())
+function playRound (humanChoice, computerChoice) {
+   if (humanChoice == computerChoice ) {
+        console.log('Pareggio');
+   } else if (( humanChoice == 's' && computerChoice == 'f' ) 
+        ||   ( humanChoice == 'f' && computerChoice == 'c') 
+        ||   ( humanChoice == 'c' && computerChoice == 's')) {
+            console.log('Hai vinto!');
+            humanScore = +1;
+        } else {
+            console.log('Hai perso!');
+            computerScore = +1;
+        }
+}
+
