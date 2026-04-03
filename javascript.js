4e9d6b937f097a21816e746886f6bf8c05faa21a
Pierluigi92 <pierluigi.mancino@hotmail.com>
2026-04-04 00:15:38 +0200

    added function 'playRound'

--- javascript.js
+++ javascript.js
@@ -1,42 +1,51 @@
 let humanScore = 0;
 let computerScore = 0;
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
 function getComputerChoice() {
-    return Math.floor(Math.random()* 3);
-}
-
-function getHumanChoice() {
-let scelta = prompt( "fai la tua scelta: 's' per sasso, 'c' per carta e 'f' per forbice ?")?.toLowerCase();  
-    if (scelta == 's') { 
-        return 0; 
-    } else if ( scelta == 'c' ) { 
-        return 1;
-    } else if ( scelta == 'f' ) {
-        return 2;
-    } else {
-        return console.log( 'Scelta errata!' );
-    } 
-}
-
-function playRound ( humanChoice, computerChoice ) {
-    if ( humanSelection == computerSelection ) {
-        console.log( 'Pareggio!' );
-    } else if (( humanSelection == 0 && computerSelection == 2 ) 
-        ||     ( humanSelection == 2 && computerSelection == 1 ) 
-        ||     ( humanSelection == 1 && computerSelection == 0 )) {
-            console.log( 'Hai vinto!' );
-            humanScore ++;
-    } else {
-            console.log( 'Hai perso!' );
-            computerScore ++;
+    let numRand = Math.floor(Math.random()* 3);
+        if ( numRand == 0 ) { 
+        return 'sasso'; 
+    } else if ( numRand == 1 ) { 
+        return 'carta';
+    } else if ( numRand == 2 ) {
+        return 'forbici';
     }
 }
 
-playRound(humanSelection, computerSelection);
+function getHumanChoice() {
+let scelta = prompt( "fai la tua scelta: ' s ' per sasso, ' c ' per carta e ' f ' per forbice ?")?.toLowerCase();  
+    if (scelta == 's') { 
+        return 'sasso'; 
+    } else if ( scelta == 'c' ) { 
+        return 'carta';
+    } else if ( scelta == 'f' ) {
+        return 'forbici';
+    }
+}
 
-console.log(('Il tuo punteggio:'), humanScore);
-console.log(('Punteggio computer:'), computerScore);
+function playRound ( humanChoice, computerChoice ) {
+    if ( humanChoice == computerChoice ) {
+    } else if (( humanChoice == 'sasso' && computerChoice == 2 ) 
+        ||     ( humanChoice == 'forbici' && computerChoice == 1 ) 
+        ||     ( humanChoice == 'carta' && computerChoice == 0 )) {
+        humanScore ++;            
+    } else {
+        computerScore ++;
+    }
+}
 
+function playGame () {
+    for ( let round = 0; round < 5; round++ ) { 
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+}
+  
+if ( humanScore > computerScore) {
+    console.log('Hai vinto la partita!');
+} else if ( computerScore > humanScore) {
+    console.log('Hai perso la partita!');
+} else 
+    console.log('Partita pareggiata!');
+}
+playGame();
