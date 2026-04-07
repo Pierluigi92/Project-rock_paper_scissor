296677d89d6014980b1cbf732e204b25c1b81b37
Pierluigi92 <pierluigi.mancino@hotmail.com>
2026-04-07 19:47:18 +0200

    modifica alla funzione 'getHumanChoice'.

--- javascript.js
+++ javascript.js
@@ -1,51 +1,57 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     let numRand = Math.floor(Math.random()* 3);
-        if ( numRand == 0 ) { 
+    if ( numRand === 0 ) { 
         return 'sasso'; 
-    } else if ( numRand == 1 ) { 
+    } else if ( numRand === 1 ) { 
         return 'carta';
-    } else if ( numRand == 2 ) {
+    } else if ( numRand === 2 ) {
         return 'forbici';
     }
 }
 
 function getHumanChoice() {
-let scelta = prompt( "fai la tua scelta: ' s ' per sasso, ' c ' per carta e ' f ' per forbice ?")?.toLowerCase();  
+    let scelta = prompt( "Fai la tua scelta: ' s ' per sasso, ' c ' per carta e ' f ' per forbice ?")?.toLowerCase();  
     if (scelta == 's') { 
         return 'sasso'; 
-    } else if ( scelta == 'c' ) { 
+    } else if ( scelta === 'c' ) { 
         return 'carta';
-    } else if ( scelta == 'f' ) {
+    } else if ( scelta === 'f' ) {
         return 'forbici';
+    } else {     
+       alert( "Scelta non valida, inserisci ' s ', ' c ', o ' f '." );
+       return getHumanChoice();
     }
 }
 
-function playRound ( humanChoice, computerChoice ) {
-    if ( humanChoice == computerChoice ) {
-    } else if (( humanChoice == 'sasso' && computerChoice == 2 ) 
-        ||     ( humanChoice == 'forbici' && computerChoice == 1 ) 
-        ||     ( humanChoice == 'carta' && computerChoice == 0 )) {
-        humanScore ++;            
-    } else {
-        computerScore ++;
-    }
-}
+let humanScore = 0;
+let computerScore = 0;
 
 function playGame () {
     for ( let round = 0; round < 5; round++ ) { 
         const humanSelection = getHumanChoice();
         const computerSelection = getComputerChoice();
         playRound(humanSelection, computerSelection);
+    }
+function playRound ( humanChoice, computerChoice ) {
+    if ( humanChoice == computerChoice ) {
+        alert( `${humanChoice} e ${computerChoice}, stessa scelta, round pareggiato!\nIl tuo punteggio: ${humanScore}\nPunteggio computer: ${computerScore}` );
+    } else if (( humanChoice === 'sasso' && computerChoice === 'forbici' ) 
+        ||     ( humanChoice === 'forbici' && computerChoice === 'carta' ) 
+        ||     ( humanChoice === 'carta' && computerChoice === 'sasso' )) {
+        humanScore ++;
+        alert( `Hai vinto il round! ${humanChoice} batte ${computerChoice}\nIl tuo punteggio: ${humanScore}\nPunteggio computer: ${computerScore}` );            
+    } else {
+        computerScore ++;
+        alert( `Hai perso il round! ${computerChoice} batte ${humanChoice}\nIl tuo punteggio: ${humanScore}\nPunteggio computer: ${computerScore}` );
+    }
 }
-  
+
 if ( humanScore > computerScore) {
-    console.log('Hai vinto la partita!');
+    alert( `Hai vinto la partita!\nIl tuo punteggio: ${humanScore}\nPunteggio computer: ${computerScore}` );
 } else if ( computerScore > humanScore) {
-    console.log('Hai perso la partita!');
+    alert( `Hai perso la partita!\nIl tuo punteggio: ${humanScore}\nPunteggio computer: ${computerScore}` );
 } else 
-    console.log('Partita pareggiata!');
+    alert( `Partita pareggiata!\nIl tuo punteggio: ${humanScore}\nPunteggio computer: ${computerScore}` );
 }
+
 playGame();
